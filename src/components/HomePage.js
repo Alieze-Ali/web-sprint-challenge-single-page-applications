9b147418f8a09241fab3d83b9a57cd0069a385f3
Alieze-Ali <71855821+Alieze-Ali@users.noreply.github.com>
2021-07-18 12:02:45 -0700

    added some state in form file

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -7,10 +7,14 @@ export default function HomePage () {
         <div className="Home">
             <NavLink to={"/"}>
                 <h1>Home Page</h1>
+                   
             </NavLink>
 
             <Link to={"/pizza"}>
                 <h2>Click Here to Build Your Primo Pizza!</h2>
+                <div>
+                        <button>Build Pizza</button>
+                    </div>
             </Link>
         </div>
         </>
--- src/components/PizzaForm.js
+++ src/components/PizzaForm.js
@@ -1,4 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+//import * as yup from 'yup';
+
+// Set validation schema later!!!
 
 // Create variables
 
@@ -14,7 +18,14 @@ const [formState, setFormState] = useState({
     basil: false,
     special: ""
 });
-
+const [errorState, setErrorState] = useState({
+    name: "",
+    size: "",
+    mushroom: "",
+    pepperoni: "",
+    basil: "",
+    special: ""
+});
 // return Form
 return (
 <>
@@ -24,34 +35,113 @@ return (
 
 <form id="pizza-form" className="pizza-form" >
     <div>
-        <label>Name
-            <input id="name-input" type="text" name="name" />
+        <label> 
+            <h3>Your Name</h3>
+            <input id="name-input" type="text" name="name" placeholder="Name" value="name" onChange="onChange"/>
         </label>
     </div>
 
     <div>
-        <label> Choice of Size </label>
-            <select name="size" value="" onchange="">
+        <label htmlFor="size"> 
+            <h3>Choice of Size</h3> 
+            <select id="size-dropdown" name="size" value="" onchange="">
                 <option value="small">Small</option>
                 <option value="medium">Medium</option>
                 <option value="large">Large</option>
             </select>
+        </label>
+           
     </div>
 
     <div>
-        <label> Choice of Sauce </label>
+        <label htmlFor="sauce"> 
+            <h3>Choice of Sauce</h3>
+                <label> 
+                <input type="radio"
+                value="Original Red"
+                checked=""
+                onChange=""
+                />
+                Original Red
+                </label>
+                <br/>
+
+                <label> 
+                <input type="radio"
+                value="Original Red"
+                checked=""
+                onChange=""
+                />
+                Spinach Alfredo
+                </label>
+                <br/>
+
+                <label> 
+                <input type="radio"
+                value="Original Red"
+                checked=""
+                onChange=""
+                />
+                BBQ Sauce
+                </label>
+                <br/>
+
+                <label> 
+                <input type="radio"
+                value="Original Red"
+                checked=""
+                onChange=""
+                />
+                Garlic Ranch
+                </label>
+                <br/>
+        </label>
     </div>
 
     <div> 
-        <label> Add Toppings </label>
+        <label> 
+            <h3>Add Toppings</h3> 
+                <label>
+                    <input type="checkbox" name="toppings" onchange="" value="" />
+                    Pepperoni
+                </label>
+                <br />
+
+                <label>
+                    <input type="checkbox" name="toppings" onchange="" value="" />
+                    Mushroom
+                </label>
+                <br />
+
+                <label>
+                    <input type="checkbox" name="toppings" onchange="" value="" />
+                    Basil
+                </label>
+                <br />
+
+                <label>
+                    <input type="checkbox" name="toppings" onchange="" value="" />
+                    Special
+                </label>
+                <br />
+        </label>
     </div>
 
     <div>
-        <label> Choice of Substitue </label>
+        <label> 
+            <h3>Choice of Substitue</h3>
+                <p>
+                    <input type="checkbox" name="substitute" onchange="" value="" />
+                    Choose up to 1
+                </p>
+        </label>
     </div>
 
     <div>
-    <label> Special Instructions </label>
+    <label> 
+        <h3>Special Instructions</h3> 
+        <input type="type" id="special-text" onchange="" value="" />
+    </label>
     </div>
 
 </form>
